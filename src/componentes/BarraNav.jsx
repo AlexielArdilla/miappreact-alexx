0fd08aebd5c9edca6f1e8c1d856e87468df6f9a0
Alejandro G. Vera <eldiosvolador@hotmail.com>
2023-06-03 00:27:25 -0300

    creando la navbar

--- src/componentes/BarraNav.jsx
+++ src/componentes/BarraNav.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import './BarraNav.css';
 
 function BarraNav() {
 
     return (
         <div>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis molestias beatae harum debitis atque, aspernatur quasi totam similique. Minus tempora officia, corporis excepturi vel autem esse saepe quisquam. Quos, corporis?
-                Ad saepe harum quis esse voluptatem aspernatur possimus, ratione asperiores assumenda aliquid aut adipisci eius! Ratione ex, temporibus fugiat inventore a quasi illum, sint eos deleniti ipsum, aliquam animi sapiente.
-                Odit saepe sunt aliquam illum animi aspernatur ipsam voluptatum fugit iste optio ut dolores labore deserunt cumque, impedit incidunt voluptatibus repellendus totam. Vitae culpa soluta praesentium sit? Nam, eos molestias.
-                Odio odit quo inventore obcaecati a minus laborum autem voluptate repudiandae necessitatibus, animi error explicabo iste. Earum adipisci error culpa harum reiciendis, veritatis totam, aperiam ratione, illum dignissimos odit nihil.
-            </p>
+            <nav>
+                <ul>
+                    <li><a href="./index.html">Home</a></li>
+                    <li><a href="./registro.html">Registro</a></li>
+                    <li><a href="./login.html">Login</a></li>
+                </ul>
+            </nav>
         </div>
     );
 }
---
+++ src/paginas/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BarraNav from "../componentes/BarraNav";
+
+function Home() {
+    
+    return(
+        <div>
+            <BarraNav/>
+        </div>
+    );
+
+}
+
+export default Home;
